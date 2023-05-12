d59c1ab8eb2d02127a28c65bf0c6190c546d8141
PedroJoMa <pejomadejefi@gmail.com>
2023-05-12 01:31:52 -0300

    Implementa a classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from collections import Counter
+
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data):
+        now = datetime.now().date()
+        fac_date = [d_factory["data_de_fabricacao"] for d_factory in data]
+        validities = [valid["data_de_validade"] for valid in data]
+        validities_formated = [
+            datetime.strptime(item, "%Y-%m-%d").date() for item in validities
+        ]
+        handle_validities = []
+        for validity in validities_formated:
+            if validity > now:
+                handle_validities.append(validity)
+        comp_more_p = Counter([comp["nome_da_empresa"] for comp in data])
+        return f"""Data de fabricação mais antiga: {min(fac_date)}
+Data de validade mais próxima: {min(handle_validities)}
+Empresa com mais produtos: {comp_more_p.most_common(1)[0][0]}"""
