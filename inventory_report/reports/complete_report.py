23b898cbcb7e6390b559d16bc559bde92d895f72
PedroJoMa <pejomadejefi@gmail.com>
2023-05-12 15:36:29 -0300

    Implementa a classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,49 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        report = SimpleReport.generate(data)
+        companies_quantity = Counter(
+            [company["nome_da_empresa"] for company in data]
+        )
+        report += "\nProdutos estocados por empresa:\n"
+        for name, quantity in companies_quantity.items():
+            report += f"- {name}: {quantity}\n"
+        return report
+
+
+mock = [
+    {
+        "id": "1",
+        "nome_do_produto": "Nicotine Polacrilex",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2021-02-18",
+        "data_de_validade": "2023-09-17",
+        "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+        "instrucoes_de_armazenamento": "instrucao 1",
+    },
+    {
+        "id": "2",
+        "nome_do_produto": "fentanyl citrate",
+        "nome_da_empresa": "Target Corporation",
+        "data_de_fabricacao": "2020-12-06",
+        "data_de_validade": "2023-12-25",
+        "numero_de_serie": "FR29 5951 7573 74OY XKGX 6CSG D20",
+        "instrucoes_de_armazenamento": "instrucao 2",
+    },
+    {
+        "id": "3",
+        "nome_do_produto": "NITROUS OXIDE",
+        "nome_da_empresa": "Galena Biopharma",
+        "data_de_fabricacao": "2020-12-22",
+        "data_de_validade": "2024-11-07",
+        "numero_de_serie": "CZ09 8588 0858 8435 9140 2695",
+        "instrucoes_de_armazenamento": "instrucao 3",
+    },
+]
+
+p1 = CompleteReport()
+print(p1.generate(mock))
